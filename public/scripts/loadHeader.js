6db5ce64faff3864f765984d8eaca4a314ec4c89
joe-tripodi <au.joseph.tripodi@gmail.com>
2024-02-04 20:14:34 +0530

    project reset

--- public/scripts/loadHeader.js
+++
@@ -1,9 +0,0 @@
-// loadHeader.js
-document.addEventListener('DOMContentLoaded', function() {
-    // Fetch and inject the header content
-    fetch('../header.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('header-container').innerHTML = html;
-        });
-});
