5abf4d8436a7eda9a574ef630f82d45865f3a021
joe-tripodi <au.joseph.tripodi@gmail.com>
2024-02-05 16:35:49 +0530

    Create the base layout for the cards #5

--- public/scripts/blurOnScroll.js
+++ public/scripts/blurOnScroll.js
@@ -1,11 +1,11 @@
 let lastKnownScrollPosition = 0;
 
-document.addEventListener("scroll", (event) => {
+document.addEventListener("scroll", () => {
   let nav = document.getElementsByClassName("navigation__list")[0];
 
   if(window.scrollY > 50 && !nav.classList.contains("blur-background")){
     nav.classList.add("blur-background");
-  } else if (window.scrollY < 50){
+  } else if (window.scrollY < 100){
     nav.classList.remove("blur-background");
   }
 });
