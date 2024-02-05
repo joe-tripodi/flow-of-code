fb01cb0e46ce86bd5a1e665227bfadc24f6855c4
joe-tripodi <au.joseph.tripodi@gmail.com>
2024-02-05 12:30:50 +0530

    Add blur and stick to top on scroll #1

---
+++ public/scripts/blurOnScroll.js
@@ -0,0 +1,11 @@
+let lastKnownScrollPosition = 0;
+
+document.addEventListener("scroll", (event) => {
+  let nav = document.getElementsByClassName("navigation__list")[0];
+
+  if(window.scrollY > 50 && !nav.classList.contains("blur-background")){
+    nav.classList.add("blur-background");
+  } else if (window.scrollY < 50){
+    nav.classList.remove("blur-background");
+  }
+});
